1827d6084599e7b3d28f91295394b47ab9f14a26
Marco Willems (M17.1) <progreon@gmail.com>
2016-06-15 02:41:21 +0200

    Fixed licensing.

--- src/redrouter/data/Location.java
+++ src/redrouter/data/Location.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 marco
+ * Copyright (C) 2016 Marco Willems
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -19,7 +19,7 @@ package redrouter.data;
 
 /**
  *
- * @author marco
+ * @author Marco Willems
  */
 public class Location {
     
--- src/redrouter/view/DVCalculatorPanel.java
+++ src/redrouter/view/DVCalculatorPanel.java
@@ -1,14 +1,25 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2016 Marco Willems
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 package redrouter.view;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
-import java.awt.event.KeyEvent;
 import javax.swing.AbstractAction;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -20,7 +31,7 @@ import redrouter.data.RouteFactory;
 
 /**
  *
- * @author marco
+ * @author Marco Willems
  */
 public class DVCalculatorPanel extends JPanel {
 
